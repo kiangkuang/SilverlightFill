614d211ce19a9788805c1d69b2a9d560ca7e9b11
kiangkuang <kiangkuang@gmail.com>
2014-05-16 17:17:47 +0800

    GUI buttons

--- SilverlightFill/MainPage.xaml.cs
+++ SilverlightFill/MainPage.xaml.cs
@@ -14,9 +14,81 @@ namespace SilverlightFill
 {
     public partial class MainPage : UserControl
     {
+        public Color color = Colors.Red;
+        public Boolean fillmode = false;
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void buttonRed(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Red);
+            color = Colors.Red;
+        }
+
+        private void buttonOrange(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Orange);
+            color = Colors.Orange;
+        }
+
+        private void buttonYellow(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Yellow);
+            color = Colors.Yellow;
+        }
+        private void buttonGreen(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Green);
+            color = Colors.Green;
+        }
+        private void buttonBlue(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Blue);
+            color = Colors.Blue;
+        }
+        private void buttonMagenta(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Magenta);
+            color = Colors.Magenta;
+        }
+        private void buttonPurple(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Purple);
+            color = Colors.Purple;
+        }
+        private void buttonBlack(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.Black);
+            color = Colors.Black;
+        }
+        private void buttonWhite(object sender, RoutedEventArgs e)
+        {
+            replaceBox.Fill = new SolidColorBrush(Colors.White);
+            color = Colors.White;
+        }
+
+        private void clear(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
+        }
+
+        private void fill(object sender, RoutedEventArgs e)
+        {
+            if (fillmode == false)
+            {
+                fillButton.Content = "Ink";
+                fillmode = true;
+            }
+            else
+            {
+                fillButton.Content = "Fill";
+                fillmode = false;
+            }
+        }
+
+
+
     }
 }
