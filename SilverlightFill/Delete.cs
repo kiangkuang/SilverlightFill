20a8ef328ad94bfa845763fa8b28acc8bd595c1b
jackyhsuch <jackyhsu0102@hotmail.com>
2014-06-06 17:24:23 +0800

    Implemented delete ink

--- SilverlightFill/Delete.cs
+++ SilverlightFill/Delete.cs
@@ -11,7 +11,7 @@ namespace SilverlightFill
 	{
 		public static void down(MouseButtonEventArgs e)
 		{
-
+			
 		}
 
 		public static void move(MouseEventArgs e)
@@ -21,7 +21,29 @@ namespace SilverlightFill
 
 		public static void up(MouseButtonEventArgs e, InkPresenter inkCanvas, Grid LayoutRoot)
 		{
+			int clickedLayer = Common.hitTestLayer(e, inkCanvas);
+			
+			StylusPointCollection spc = new StylusPointCollection();
+			
+			spc.Add(new StylusPoint(e.GetPosition(inkCanvas).X, e.GetPosition(inkCanvas).Y));
+			if (inkCanvas.Strokes.HitTest(spc).Count > 0)
+			{
+				for (int i = 0; i < inkCanvas.Strokes.HitTest(spc).Count; i++)
+				{
+					inkCanvas.Strokes.Remove(inkCanvas.Strokes.HitTest(spc)[i]);
+				}
 
+				return;
+			}
+
+			if (clickedLayer == -1)
+			{
+				return;
+			}
+
+			MainPage.wbList.RemoveAt(clickedLayer);
+			LayoutRoot.Children.Remove(MainPage.imageList[clickedLayer]);
+			MainPage.imageList.RemoveAt(clickedLayer);
 		}
 	}
 }
