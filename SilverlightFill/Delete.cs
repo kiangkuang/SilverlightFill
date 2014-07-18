1880f13bb98df264d0e77475a8ee015852bdc2db
jackyhsuch <jackyhsu0102@hotmail.com>
2014-07-18 16:41:50 +0800

    Refactor Delete

--- SilverlightFill/Delete.cs
+++ SilverlightFill/Delete.cs
@@ -22,25 +22,42 @@ namespace SilverlightFill
 		public static void up(MouseButtonEventArgs e, InkPresenter inkCanvas, Grid LayoutRoot)
 		{
 			int clickedLayer = Common.hitTestLayer(e, inkCanvas);
-			
 			StylusPointCollection spc = new StylusPointCollection();
-			
-			spc.Add(new StylusPoint(e.GetPosition(inkCanvas).X, e.GetPosition(inkCanvas).Y));
-			if (inkCanvas.Strokes.HitTest(spc).Count > 0)
-			{
-				for (int i = 0; i < inkCanvas.Strokes.HitTest(spc).Count; i++)
-				{
-					inkCanvas.Strokes.Remove(inkCanvas.Strokes.HitTest(spc)[i]);
-				}
 
-				return;
-			}
+			checkIfStrokeSelected(e, inkCanvas, spc);
 
+			checkIfLayerSelected(LayoutRoot, clickedLayer);
+		}
+
+		private static void checkIfLayerSelected(Grid LayoutRoot, int clickedLayer)
+		{
 			if (clickedLayer == -1)
 			{
 				return;
 			}
 
+			removeSelectedLayer(LayoutRoot, clickedLayer);
+		}
+
+		private static void checkIfStrokeSelected(MouseButtonEventArgs e, InkPresenter inkCanvas, StylusPointCollection spc)
+		{
+			spc.Add(new StylusPoint(e.GetPosition(inkCanvas).X, e.GetPosition(inkCanvas).Y));
+
+			if (inkCanvas.Strokes.HitTest(spc).Count > 0)
+			{
+				removeSelectedStroke(inkCanvas, spc);
+			}
+
+			return;
+		}
+
+		private static void removeSelectedStroke(InkPresenter inkCanvas, StylusPointCollection spc)
+		{
+			inkCanvas.Strokes.Remove(inkCanvas.Strokes.HitTest(spc)[inkCanvas.Strokes.HitTest(spc).Count - 1]);
+		}
+
+		private static void removeSelectedLayer(Grid LayoutRoot, int clickedLayer)
+		{
 			MainPage.wbList.RemoveAt(clickedLayer);
 			LayoutRoot.Children.Remove(MainPage.imageList[clickedLayer]);
 			MainPage.imageList.RemoveAt(clickedLayer);
